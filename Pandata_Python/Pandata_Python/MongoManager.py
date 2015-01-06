c0e97951253551c12c872aa74b1492d47e386b78
unknown <adrien.you@student.ecp.fr>
2015-01-06 22:42:36 +0100

    Schema

--- Pandata_Python/Pandata_Python/MongoManager.py
+++ Pandata_Python/Pandata_Python/MongoManager.py
@@ -4,6 +4,17 @@
 
 import pymongo
 
+# Create a database
+def createDatabase(db_name, client):
+    db = client[db_name]
+    print("Creating database : " + db_name)
+
+# Create a collection in a given database and a client
+def createCollection(coll_name, db_name, client):
+    db = client[db_name]
+    collection = db[coll_name]
+    print("Creating collection : " + coll_name)
+
 # Insert a document in a given collection in a database and a client
 def insertDocInCollection(doc, coll_name, db_name, client):
     db = client[db_name]
--- Pandata_Python/Pandata_Python/Pandata_Python.py
+++ Pandata_Python/Pandata_Python/Pandata_Python.py
@@ -5,6 +5,7 @@
 import MongoManager
 import TwitterManager
 import Constants
+import pymongo
 
 
 consumer_key = Constants.Connexion.CONSUMER_KEY
@@ -12,14 +13,23 @@ consumer_secret = Constants.Connexion.CONSUMER_SECRET
 access_token = Constants.Connexion.ACCESS_TOKEN_KEY
 access_token_secret = Constants.Connexion.ACCESS_TOKEN_SECRET
 
+doc = {}
+doc["_id"] = 1
+doc["name"] = 'Adrien You'
+
 if __name__ == '__main__':
 
     # Connect to the default host and port. Can be specified : MongoClient('localhost', 27017)
-    #client = pymongo.MongoClient()
+    client = pymongo.MongoClient()
+
+    # Create db, coll et insert car si pas de docs, la db et la coll ne sont pas save
+    MongoManager.createDatabase("premierTest" , client)
+    MongoManager.createCollection("premierCollTest", "premierTest", client)
+    MongoManager.insertDocInCollection(doc, "premierCollTest", "premierTest", client)
     
     # Create the stream with a filter on the following tracks
-    streamer = TwitterManager.TweetStreamer(consumer_key, consumer_secret, access_token, access_token_secret)
-    streamer.statuses.filter(track = 'samsung, apple', language = 'en, fr')
+    #streamer = TwitterManager.TweetStreamer(consumer_key, consumer_secret, access_token, access_token_secret)
+    #streamer.statuses.filter(track = 'samsung, apple', language = 'en, fr')
     
     
 
