2d063d56c3ac807cf77bd8dacbbaf08836430d17
AYouEcp <adrien.you@student.ecp.fr>
2015-03-12 16:59:10 +0100

    add readme

--- Pandata_Python/Pandata_Python/Variables.py
+++ Pandata_Python/Pandata_Python/Variables.py
@@ -7,12 +7,16 @@ class Database:
     """Contains the Database constants, such as db_name and coll_name"""
     """Contains the Database varaibles, such as python_id, existing_coll and user_id"""
 
+    # Do not change this
     DB_NAME = "mean-dev"
     COLL_NAME = "researches"
-    RESEARCH_PYTHON_ID = 3 # Need to increment it everytime
-    EXISTING_RESEARCH = False
+
+    # Change this
+    RESEARCH_PYTHON_ID = 2 # Need to increment it everytime
+    # Put to true if the current stream failed and you want to start it again. Else, let it at false.
+    EXISTING_RESEARCH = True
     RESEARCH_USER_ID = "54689fc0b456057800046d6f" #{ "$oid" : "54689fc0b456057800046d6f" } 
-    RESEARCH_TITLE = "Microsoft and Windows 10, research 2"
-    RESEARCH_DESC = "This is a second research about Microsoft and Windows 10 over Twitter only"
+    RESEARCH_TITLE = "Barack Obama"
+    RESEARCH_DESC = "This is a research about Barack Obama"
     DURATION = 3
-    WORDS = ["microsoft", "windows", "windows10"]
+    WORDS = ["obama", "barack", "barackobama"]
